135f9da0604f3d468cc4443413d0110484d1914a
Stefan Iarca <stefan@Stefans-MacBook-Pro.local>
2018-03-20 00:43:23 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='azure_chestxray_cam',
+      version='1',
+      description='Package 1',
+      author='Stefan Iarca',
+      author_email='iasotm@yahoo.com',
+      url='https://github.com/Stexan/xvisionServer',
+      license='MIT',
+      install_requires=['keras'],
+      classifiers=[
+                   '',
+                  ]
+                   ],
+      packages=find_packages())
